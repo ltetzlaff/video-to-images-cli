c2506bb8e4cac3bc139ab10ed0ff4814d151d439
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-08 22:56:17 +0200

    :hammer: improve test execution

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,16 +1,31 @@
 import { exec } from "../util"
 
 async function run() {
-  const commands = ["node dist/cli.js"]
+  const commands = ["node dist/cli.js --help"]
   const args = ""
 
   const res = await Promise.all(
-    commands.map(async (c, i) => exec(c + args)).map(async (execution, i) => {
-      const { stdout, stderr } = await execution
-      return { stdout, stderr }
-    })
+    commands
+      .map(async (c, i) => exec(`${c} ${args}`))
+      .map(async (execution, i) => {
+        const { stdout, stderr } = await execution
+        return { stdout, stderr }
+      })
   )
-  console.log(JSON.stringify(res))
+  for (let i = 0; i < commands.length; i++) {
+    const r = res[i]
+    const c = commands[i]
+
+    console.log(
+      [
+        `\ncommand: ${c}`,
+        "stdout:",
+        r.stdout,
+        "stderr:",
+        r.stderr ? r.stderr : "undefined"
+      ].join("\n  ")
+    )
+  }
 }
 
 run()
